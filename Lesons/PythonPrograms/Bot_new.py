c6face223d71aa228c69b8a4d631505b380ce948
Frozn_11 <karim.elkhawankey@gmail.com>
2023-08-18 17:27:16 +0300

    NowEvenMoreUpdete

--- Lesons/PythonPrograms/Bot_new.py
+++ Lesons/PythonPrograms/Bot_new.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
-from bot_logic import gen_pass
+from bot_logic import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -20,9 +20,26 @@ async def pasw(ctx, pass_length=10):
     await ctx.send(gen_pass(pass_length))
 
 @bot.command()
+async def divide(ctx, Number1: float, Number2: float):
+    await ctx.send(divideC(Number1, Number2))
+    
+@bot.command()
+async def Add(ctx, Number1: float, Number2: float):
+    await ctx.send(AddC(Number1, Number2))
+
+@bot.command()
+async def Subtract(ctx, Number1: float, Number2: float):
+    await ctx.send(SubtractC(Number1, Number2))
+
+@bot.command()
+async def Multiply(ctx, Number1: float, Number2: float):
+    await ctx.send(MultiplyC(Number1, Number2))
+
+@bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)  
 
-bot.run("MTExNDIyOTE4NDUyNTUwODY1OQ.GJ6jSB.bM7bJXZuxYUDlKGBVeNCxRW6pR8cySyEjvXMU8")
+
+bot.run("Tonke")
--- Lesons/PythonPrograms/bot_logic.py
+++ Lesons/PythonPrograms/bot_logic.py
@@ -15,6 +15,18 @@ def gen_emodji():
     emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923",":nerd:"]
     return random.choice(emodji)
 
+def divideC(Number1, Number2):
+    return Number1 / Number2 
+
+def AddC(Number1, Number2):
+    return Number1 + Number2 
+
+def SubtractC(Number1, Number2):
+    return Number1 - Number2 
+
+def MultiplyC(Number1, Number2):
+    return Number1 * Number2 
+
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>"
     password = ""
